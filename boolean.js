3512db123e355919353b1eb96e874d3f4f2cf8fe
RomuloTolfo <romulo_tolfo@hotmail.com>
2022-07-25 15:57:28 -0300

    aula 1

---
+++ boolean.js
@@ -0,0 +1,9 @@
+
+//boolean
+
+const primeiroNumero = 5
+const segundoNumero = 5
+
+console.log(primeiroNumero === segundoNumero);
+
+
---
+++ type-number.js
@@ -0,0 +1,29 @@
+// tipo number
+
+const meuNumero = 3;
+
+const primeiroNumero = 1;
+const segundoNumero = 2;
+
+// const operacaoMatematica = primeiroNumero + segundoNumero;
+
+// console.log(operacaoMatematica);
+
+// ponto flutuante
+
+const numeroPontoFlutuante = 6.3;
+const pontoFlutuanteSemZero = 3;
+
+const novaOperacao = primeiroNumero/numeroPontoFlutuante;
+
+console.log(novaOperacao) 
+
+
+// strings
+
+const citacao = "Meu nome é "
+const nome = "Rômulo" 
+
+console.log(citacao + nome)
+
+
