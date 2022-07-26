49d97853a106fde939c424e7591d62f0bd1cb6a2
RomuloTolfo <romulo_tolfo@hotmail.com>
2022-07-26 15:38:33 -0300

    expressoes funcao

---
+++ expressoesfuncao.js
@@ -0,0 +1,11 @@
+function minhaFuncao(param) {
+    //bloco de codigo
+}
+
+minhaFuncao("param")
+
+//expressao de funcao
+
+
+const soma = function(num1, num2) {return num1 + num2}
+console.log(soma(1, 1))
---
+++ parametros.js
@@ -0,0 +1,19 @@
+//function soma(num1, num2) {
+  //  return num1 + num2;
+//}
+
+//console.log (soma(2, 2))
+//console.log (soma(-200, 2))
+//console.log (soma(-400, 2))
+
+
+function soma(numero1, numero2) {
+    return numero1 + numero2;
+}
+
+function multiplica(numero1 = 1, numero2 = 1) {
+    return numero1 * numero2;
+}
+
+console.log(multiplica(soma(4,5)))
+
