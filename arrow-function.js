eb24d83a49796e0d94a2b8db87e54b01fd89f847
RomuloTolfo <romulo_tolfo@hotmail.com>
2022-07-27 15:16:52 -0300

    arrays

--- arrow-function.js
+++ arrow-function.js
@@ -1,2 +1,13 @@
-const apresenteArrow = nome => `meu nome é ${nome}`;
-const soma = (num1, num2) => num1 + num 2;
+// const apresenteArrow = nome => `meu nome é ${nome}`;
+// const soma = (num1, num2) => num1 + num 2;
+
+// arrow function com mais de 1 linha
+
+const somaNumerosPequenos = (num1, num2) => {
+    if (num1 > 10 || num2 > 10) {
+        return "somente números de 1 a 9"
+    } else {
+        return num1 + num2;
+    }
+}
+
